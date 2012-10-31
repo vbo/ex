0e2d1c424c08d22d4ee95d3f9d5aba8010f7b4d3
Borodin Vadim <borodin.vadim@gmail.com>
2012-10-31 04:54:18 +0400

    rbtree: insert routine and rebalancing

--- dsa/rbtree.cpp
+++ dsa/rbtree.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 
-// TMP
-template <class T>
-void pr(T m) {
-    std::cout << m << std::endl;
-}
-
 enum Color { RED, BLACK };
 
 struct Node {
@@ -21,6 +15,7 @@ struct Node {
         }
         return parent->parent;
     }
+
     Node* uncle() {
         Node *grand = grandparent();
         if (!grand) {
@@ -33,166 +28,173 @@ struct Node {
     }
 };
 
-void dump(Node* root, std::string offset = "") {
-    if (!root) {
-        return;
-    }
-    dump(root->right, offset + "r\t");
-    std::cout << offset << (root->color == BLACK ? "b" : "r") << root->data << std::endl;
-    dump(root->left, offset + "l\t");
-}
+class RBTree {
+    Node* root;
 
-void rotate_right(Node* n) {
-    pr("rotating over");
-    pr(n->data);
-    Node *pivot = n->left;
-    n->left = pivot->right;
-    pivot->right = n;
-    pivot->parent = n->parent;
-    n->parent = pivot;
-    Node *parent = pivot->parent;
-    if (!parent) return;
-    if (parent->left == n) {
-        parent->left = pivot;
-    } else {
-        parent->right = pivot;
-    }
-}
+public:
+    RBTree() : root(NULL) {}
+    ~RBTree() { free(root); }
 
-void rotate_left(Node* n) {
-    pr("rotating over");
-    pr(n->data);
-    Node *pivot = n->right;
-    n->right = pivot->left;
-    pivot->left = n;
-    pivot->parent = n->parent;
-    n->parent = pivot;
-    Node *parent = pivot->parent;
-    if (!parent) return;
-    if (parent->left == n) {
-        parent->left = pivot;
-    } else {
-        parent->right = pivot;
+    void dump() {
+        return dump(root, "");
     }
-}
 
-// Rebalance tree afte insert
-// Warn: tree head may be actually changed after rebalance
-void balance(Node* n) {
-    n->color = RED;
-    if (!n->parent) {
-        // It's a tree head
-        pr("head!");
-        n->color = BLACK;
-        return;
+    Node* insert(const int data) {
+        return insert(&(root), data, NULL);
     }
-    if (n->parent->color == BLACK) {
-        // tree is still valid
-        pr("tree valid");
-        return;
-    }
-    Node *u = n->uncle();
-    Node *g = n->grandparent();
-    if (u && u->color == RED) {
-        // parent and uncle are RED
-        pr("parent and uncle are RED");
-        n->parent->color = BLACK;
-        u->color = BLACK;
-        g->color = RED;
-        balance(g);
-        return;
-    }
-    // RED parent and BLACK uncle
-    pr("RED parent and BLACK uncle");
-    // Perform rotation to line-up red nodes if needed
-    if ((n == n->parent->left) && (n->parent == g->right)) {
-        pr("right!!!");
-        rotate_right(n->parent);
-        n = n->right;
-    } else if ((n == n->parent->right) && (n->parent == g->left)) {
-        pr("left!!!");
-        rotate_left(n->parent);
-        n = n->left;
-    }
-    // Nodes are lined-up
-    // Rotate grandparent
-    pr("rotation after line-up");
-    n->parent->color = BLACK;
-    g->color = RED;
-    if (n == n->parent->left) {
-        pr("right");
-        rotate_right(g);
-    } else {
-        pr("left");
-        rotate_left(g);
-    }
-}
 
-Node* insert(Node** root, const int data, Node* parent = NULL) {
-    if (!*root) {
-        *root = new Node();
-        Node *n = *root;
-        n->data = data;
-        n->parent = parent;
-        balance(n);
-        return n;
+    Node* find(const int data) {
+        return find(root, data);
     }
-    int val = (*root)->data;
-    if (val == data) {
-        return *root;
-    }
-    Node **place;
-    if (val < data) {
-        place = &((*root)->right);
-    } else {
-        place = &((*root)->left);
-    }
-    return insert(place, data, *root);
-}
 
-Node* find(Node* root, const int data) {
-    if (!root) {
-        return NULL;
+private:
+    Node* insert(Node** node, const int data, Node* parent) {
+        if (!*node) {
+            *node = new Node();
+            Node *n = *node;
+            n->data = data;
+            n->parent = parent;
+            balance_after_insert(n);
+            return n;
+        }
+        int val = (*node)->data;
+        if (val == data) {
+            return *node;
+        }
+        Node **place;
+        if (val < data) {
+            place = &((*node)->right);
+        } else {
+            place = &((*node)->left);
+        }
+        return insert(place, data, *node);
     }
-    int val = root->data;
-    if (val == data){
-        return root;
-    }
-    if (val < data) {
-        return find(root->right, data);
-    } else {
+
+    Node* find(Node* root, const int data) {
+        if (!root) {
+            return NULL;
+        }
+        int val = root->data;
+        if (val == data){
+            return root;
+        }
+        if (val < data) {
+            return find(root->right, data);
+        }
         return find(root->left, data);
     }
-}
+
+    void dump(Node* node, std::string offset) {
+        if (offset.length() == 0) {
+            node = this->root;
+        }
+        if (!node) {
+            return;
+        }
+        dump(node->right, offset + "r\t");
+        std::cout << offset << (node->color == BLACK ? "b" : "r") << node->data << std::endl;
+        dump(node->left, offset + "l\t");
+    }
+
+    void rotate_right(Node* n) {
+        Node *pivot = n->left;
+        n->left = pivot->right;
+        pivot->right = n;
+        pivot->parent = n->parent;
+        n->parent = pivot;
+        Node *parent = pivot->parent;
+        if (!parent) {
+            root = pivot;
+            return;
+        }
+        if (parent->left == n) {
+            parent->left = pivot;
+        }
+        parent->right = pivot;
+    }
+
+    void rotate_left(Node* n) {
+        Node *pivot = n->right;
+        n->right = pivot->left;
+        pivot->left = n;
+        pivot->parent = n->parent;
+        n->parent = pivot;
+        Node *parent = pivot->parent;
+        if (!parent) {
+            root = pivot;
+            return;
+        }
+        if (parent->left == n) {
+            parent->left = pivot;
+        }
+        parent->right = pivot;
+    }
+
+    void free(Node* n) {
+        if (!n) {
+            return;
+        }
+        free(n->left);
+        free(n->right);
+        delete n;
+    }
+
+    void balance_after_insert(Node* n) {
+        n->color = RED;
+        if (!n->parent) {
+            n->color = BLACK;
+            return;
+        }
+        if (n->parent->color == BLACK) {
+            return;
+        }
+        // If we are here our parent is RED
+        // so we probably need to repaint it
+        // Checking the uncle
+        Node *u = n->uncle();
+        Node *g = n->grandparent();
+        if (u && u->color == RED) {
+            // Parent and uncle are both RED
+            // Repainting both to black and begin a recursion
+            n->parent->color = BLACK;
+            u->color = BLACK;
+            g->color = RED;
+            balance_after_insert(g);
+            return;
+        }
+        // RED parent and BLACK(!) uncle
+        // We need to rotate it, but sometimes it can be done in two steps
+        // Perform rotation to line-up red nodes first if needed
+        if ((n == n->parent->left) && (n->parent == g->right)) {
+            rotate_right(n->parent);
+            n = n->right;
+        } else if ((n == n->parent->right) && (n->parent == g->left)) {
+            rotate_left(n->parent);
+            n = n->left;
+        }
+        // Nodes are lined-up
+        // Rotate grandparent
+        n->parent->color = BLACK;
+        g = n->grandparent();
+        g->color = RED;
+        if (n == n->parent->left) {
+            rotate_right(g);
+        } else {
+            rotate_left(g);
+        }
+    }
+};
 
 int main() {
-    Node *root = NULL;
-    pr("insert 1000");
-    insert(&root, 1000);
-    while(root->parent) root = root->parent;
-    dump(root);
-    pr("insert 1100");
-    insert(&root, 1100);
-    while(root->parent) root = root->parent;
-    dump(root);
-    pr("insert 1120");
-    insert(&root, 1120);
-    while(root->parent) root = root->parent;
-    dump(root);
-    insert(&root, 1130);
-    while(root->parent) root = root->parent;
-    dump(root);
-    insert(&root, 1200);
-    while(root->parent) root = root->parent;
-    dump(root);
-    insert(&root, 500);
-    while(root->parent) root = root->parent;
-    dump(root);
-    insert(&root, 1500);
-    while(root->parent) root = root->parent;
-    dump(root);
-    insert(&root, 1700);
-    while(root->parent) root = root->parent;
-    dump(root);
+    RBTree tree;
+    tree.insert(800);
+    tree.insert(1500);
+    tree.insert(2000);
+    tree.insert(900);
+    tree.insert(1900);
+    tree.insert(500);
+    tree.insert(5900);
+    tree.insert(8900);
+    tree.dump();
 }
 
